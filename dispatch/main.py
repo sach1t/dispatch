4c7786c9ca6b718a20f3b985b2a06608f4e06a14
sach1t <sach0010t@gmail.com>
2015-11-23 19:05:39 -0500

    change dir

--- dispatch/main.py
+++ dispatch/main.py
@@ -2,7 +2,7 @@ from dispatch.search import Searcher
 from dispatch.ui.mainwindow import MainWindow
 from gi.repository import Gtk, GObject
 from dispatch.plugin_manager import PluginManager
-
+import os 
 def read_css(filename):
     with open(filename) as f:
         data = f.read()
@@ -14,10 +14,9 @@ def main():
     plugin_manager.load_plugins()
     plugins = plugin_manager.get_plugins()
     print("Loaded Plugins : ", plugins)
-
     searcher = Searcher(plugins)
-
     style = read_css("./dispatch/ui/style.css")
+    os.chdir(os.path.expanduser("~"))
     win = MainWindow(searcher, style)
     win.show_all()
     Gtk.main()
