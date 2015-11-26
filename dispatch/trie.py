4c0facf16a89a80cafe1f0814630585a108ba002
sach1t <sach0010t@gmail.com>
2015-11-26 17:26:07 -0500

    Comment Trie

--- dispatch/trie.py
+++ dispatch/trie.py
@@ -1,23 +1,23 @@
 class TrieNode:
     def __init__(self, suffix, parent):
+        '''Create node for Trie with given suffix and parent.'''
         self.suffix = suffix
-        self.children = {}
         self.parent = parent
+        self.children = {}
         self.data = []
 
     def __str__(self):
         return self.suffix
 
-    def __repr__(self):
-        return self.suffix
-
 
 class Trie:
     def __init__(self):
+        '''Create empty Trie.'''
         self.root = TrieNode("", None)
         self.last_search = self.root
 
     def insert(self, key, data):
+        '''Insert key with corresponding data.'''
         node = self.root
         for char in key:
             if char in node.children:
@@ -27,14 +27,11 @@ class Trie:
                 node = node.children[char]
         node.data.append(data)
 
-    def find_matches(self, key, restart=True):
-        if restart:
-            node = self.search(key)
-            self.last_search = node
-        else:
-            node = self.last_search
+    def find_matches(self, key):
+        '''Return data of all nodes with suffix equal to key'''
+        node = self.search(key)
 
-        # bfs -  thus closest match should be shown first in each tree 
+        # BFS - The shortest match is shown first
         results = []
         if node is not None:
             stack = [node]
@@ -46,6 +43,7 @@ class Trie:
         return results
 
     def search(self, key):
+        '''Return node with suffix equal to given key.'''
         node = self.root
         for char in key:
             if char in node.children:
